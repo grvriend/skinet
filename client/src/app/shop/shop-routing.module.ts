27604ea66ca075eef8e56726d27307237b1ae6fa
Graham Vriend <gvriend@factsmgt.com>
2020-04-15 20:14:34 -0700

    End of section 12

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -5,7 +5,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: ShopComponent },
-  { path: ':id', component: ProductDetailsComponent },
+  {
+    path: ':id',
+    component: ProductDetailsComponent,
+    data: { breadcrumb: { alias: 'productDetails' } },
+  },
 ];
 
 @NgModule({
